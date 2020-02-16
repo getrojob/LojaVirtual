8939dffac7966550e8039fa73c6da3de86fbbfa2
Getulio Rodrigues da Silva <getulio.r.da.silva@avanade.com>
2020-02-16 19:09:36 -0300

    Add project files.

---
+++ LojaVirtual/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojaVirtual.Libraries;
+using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LojaVirtual.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ContatoAcao()
+        {
+            try
+            {
+                Contato contato = new Contato();
+                contato.Nome = HttpContext.Request.Form["nome"];
+                contato.Email = HttpContext.Request.Form["email"];
+                contato.Texto = HttpContext.Request.Form["texto"];
+
+                var ListaMensagens = new List<ValidationResult>();
+                var context = new ValidationContext(contato);
+                bool isValid = Validator.TryValidateObject(contato, context, ListaMensagens, true);
+
+                if (isValid)
+                {
+                    ContatoEmail.EnviarContatoPorEmail(contato);
+                    ViewData["MSG_S"] = "Mensagem de contato enviado com sucesso!";
+                }
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach (var texto in ListaMensagens)
+                    {
+                        sb.Append(texto.ErrorMessage + "<br/>");
+                    }
+                    ViewData["MSG_S"] = sb.ToString();
+                    ViewData["CONTATO"] = contato;
+                }
+            }
+            catch (Exception e)
+            {
+                ViewData["MSG_S"] = "Opps! Tivemos um erro, tente noveamente mais tarde!";
+                //TODO - Implementar Log
+
+            }
+
+            return View("Contato");
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
+        }
+    }
+}
---
+++ LojaVirtual/Libraries/ContatoEmail.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using LojaVirtual.Models;
+
+namespace LojaVirtual.Libraries
+{
+    public class ContatoEmail
+    {
+        public static void EnviarContatoPorEmail(Contato contato)
+        {
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("getrojob@gmail.com", "Guga3012@");
+            smtp.EnableSsl = true;
+
+            string corpoMsg = string.Format("<h2>Contato - Loja Virtual</h2>" +
+                "<b>Nome: </b> {0} <br/>"+
+                "<b>E-mail: </b> {1} <br/>" +
+                "<b>Texto: </b> {2} <br/>" +
+                "<br/>E-mail enviado automatico do site LojaVirtual.",
+                contato.Nome,
+                contato.Email,
+                contato.Texto
+                );
+
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress("getrojob@gmail.com");
+            message.To.Add("getrojob@gmail.com");
+            message.Subject = "Contato - LojaVirtual - E-mail: " + contato.Email;
+            message.Body = corpoMsg;
+            message.IsBodyHtml = true;
+
+            //Enviar mensagem
+            smtp.Send(message);
+        }
+    }
+}
